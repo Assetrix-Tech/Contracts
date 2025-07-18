67d2ba5ccbd751e60ca76c2a5f40c4816c4e2c81
Tola-byte <adetolaisaiah17@gmail.com>
2025-07-18 17:14:05 +0100

    feat: added investment module

--- contracts/Assetrix.sol
+++ contracts/Assetrix.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 import "./modules/AssetrixPropertyManagement.sol";
+import "./modules/AssetrixInvestmentManagement.sol";
 
-contract AssetrixTest is AssetrixPropertyManagement {
+contract AssetrixTest is AssetrixPropertyManagement, AssetrixInvestmentManagement {
     function initialize(address initialOwner) public initializer {
         __AssetrixPropertyManagement_init(initialOwner);
+        __AssetrixInvestmentManagement_init(initialOwner);
+
     }
 
     // function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
